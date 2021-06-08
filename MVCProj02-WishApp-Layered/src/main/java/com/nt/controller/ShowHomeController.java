a07b80773d0938aa0c817d7d6e58fb368b4f0dd0
hp <hp@venkat>
2021-06-08 14:49:33 +0530

    Layered Wish app

---
+++ MVCProj02-WishApp-Layered/src/main/java/com/nt/controller/ShowHomeController.java
@@ -0,0 +1,18 @@
+package com.nt.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class ShowHomeController implements Controller {
+
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		System.out.println("ShowHomeController.handleRequest()");
+		
+		return new ModelAndView("home");
+	}
+
+}
